6daa6edef955c2f11fe5fb8b9210442a1abb49ef
shanawazams <shanawaz.ams@gmail.com>
2023-12-06 19:13:45 +0530

    first commit

---
+++ foodstore/components/footer.js
@@ -0,0 +1,16 @@
+export const footer = () => {
+  return `<div id="footer">
+            <p class="text-center">Made by SRM Technophiles</p>
+            </div>`;
+  // <div>
+  //     <button class="btn btn-primary">
+  //         <a href="https://www.linkedin.com/in/m-sehrawat/" target="_blank">LinkedIn</a>
+  //     </button>
+  //     <button class="btn btn-success">
+  //         <a href="https://github.com/m-sehrawat" target="_blank">GitHub</a>
+  //     </button>
+  //     <button class="btn btn-danger">
+  //         <a href="https://mohit-portfolio.vercel.app/" target="_blank">Portfolio</a>
+  //     </button>
+  // </div>
+};
---
+++ foodstore/functions/getCoupon.js
@@ -0,0 +1,15 @@
+const couponCodes = {
+    FOODSTORE5: 5,
+    FOODSTORE10: 10,
+    FOODSTORE15: 15,
+    FOODSTORE20: 20,
+    FOODSTORE25: 25,
+    FOODSTORE30: 30,
+    FOODSTORE50: 50
+};
+
+
+export const getCoupon = (coupon) => {
+
+    return couponCodes[coupon];
+};
